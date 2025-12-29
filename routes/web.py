e905109ae48ccdfec3af53d4985cb0e9010c9aa5
DianKartiko <auliakartiko28@gmail.com>
2025-12-29 19:39:02 +0700

    Update Home and About Page, But Have To Fix Error in JS

--- routes/web.py
+++ routes/web.py
@@ -8,3 +8,8 @@ bp = Blueprint("route", __name__)
 @bp.route("/")
 def index():
     return render_template("menu/home.html")
+
+
+@bp.route("/about")
+def about():
+    return render_template("menu/about.html")
